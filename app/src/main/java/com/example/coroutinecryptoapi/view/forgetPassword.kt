dfb4ed0b9066476d8c862c542418ad5378b0b1df
Rasim ALTUNTAŞ <rasim0042@gmail.com>
2021-10-20 09:27:39 +0300

    ForgetUpdate-1

--- app/src/main/java/com/example/coroutinecryptoapi/view/forgetPassword.kt
+++ app/src/main/java/com/example/coroutinecryptoapi/view/forgetPassword.kt
@@ -2,11 +2,13 @@ package com.example.coroutinecryptoapi.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.coroutinecryptoapi.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_forget_password.*
 import kotlinx.android.synthetic.main.activity_forget_password.view.*
+import java.lang.Exception
 
 class forgetPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,22 +19,33 @@ class forgetPassword : AppCompatActivity() {
         }
     }
 
-    fun forgetPassword(){
+    fun forgetPassword() {
+        try {
+            val auth = FirebaseAuth.getInstance()
+            val currentEmail = forgetEmailEditText.text.toString()
+            println(currentEmail)
+            if ("@" in currentEmail) {
+                val result = auth.sendPasswordResetEmail(currentEmail)
+                result.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Eposta Adresinizi kontrol ediniz...", Toast.LENGTH_LONG)
+                        println("${currentEmail}")
+                    } else {
+                        println("E posta gönderilemedi...")
+                        Toast.makeText(this,"Eposta Gönderilemedi",Toast.LENGTH_LONG).show()
+                    }
 
-        val auth = FirebaseAuth.getInstance()
-        val currentEmail = forgetEmailEditText.text.toString()
-        println(currentEmail)
-        val result = auth.sendPasswordResetEmail(currentEmail)
-        result.addOnCompleteListener{
-            task ->
-            if(task.isSuccessful){
-                println("E postanızı Kontrol ediniz...")
+                }
+
             }
             else{
-                println("E posta gönderilemedi...")
+                println("Lütfen Geçerli bir eposta adresi giriniz...")
+                Toast.makeText(this,"Lütfen Geçerli Bir Eposta Adresi Giriniz...",Toast.LENGTH_LONG).show()
             }
 
+        }catch (e:Exception){
+            println(e.localizedMessage)
         }
-
     }
 }
+
